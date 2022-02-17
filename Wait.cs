1ba13a895e0fb77d3c4ff580952d567166d1e1c7
Kajetan Krawczyk <kaitek666@gmail.com>
2022-02-17 14:13:21 +0100

    Update Wait.cs

--- Wait.cs
+++ Wait.cs
@@ -3,43 +3,46 @@ using System.Diagnostics;
 
 namespace Whatever
 {
-    public class Wait
+    public static class Wait
     {
-        private Func<bool> _condition;
-        private bool _until;
+        public static W<T> Until<T>(Func<T> condition) => new W<T>(condition, true);
 
-        private Wait(Func<bool> condition, bool until)
+        public static W<T> AsLongAs<T>(Func<T> condition) => new W<T>(condition, false);
+
+        public class W<T>
         {
-            _condition = condition;
-            _until = until;
-        }
+            private Func<T> _condition;
+            private bool _until;
 
-        public static Wait Until(Func<bool> condition) => new(condition, true);
-
-        public static Wait AsLongAs(Func<bool> condition) => new(condition, false);
-
-        public void Is(object target, int timeout = 10)
-        {
-            if (_condition.Method.ReturnType != target.GetType())
-                throw new Exception("Can't compare variables of different type.");
-
-            if (timeout < 1)
-                throw new Exception("Timeout needs to be a positive integer");
-
-            var sw = new Stopwatch();
-            sw.Start();
-
-            while (sw.Elapsed.TotalSeconds <= 10)
+            public W(Func<T> condition, bool until)
             {
-                switch (_until)
-                {
-                    case true when _condition.Invoke().Equals(target):
-                    case false when !_condition.Invoke().Equals(target):
-                        return;
-                }
+                _condition = condition;
+                _until = until;
             }
 
-            throw new Exception($"Timed out while waiting for {_condition} to be {target}");
+            public void Is(object target, int timeout = 10)
+            {
+                if (_condition.Method.ReturnType != target.GetType())
+                    throw new Exception("Can't compare variables of different type.");
+
+                if (timeout < 1)
+                    throw new Exception("Timeout needs to be a positive integer");
+
+                var sw = new Stopwatch();
+                sw.Start();
+
+                while (sw.Elapsed.TotalSeconds <= 10)
+                {
+                    switch (_until)
+                    {
+                        case true when _condition.Invoke().Equals(target):
+                        case false when !_condition.Invoke().Equals(target):
+                            return;
+                    }
+                }
+
+                throw new Exception($"Timed out while waiting for {_condition} to be {target}");
+            }
         }
     }
 }
